c689f586c059debffac38874cf839ded6ba50504
Björn Petersen <pheasn@gmail.com>
2013-11-07 18:36:21 +0100

    Fixed auto saver saving sql ownings probably finally fixes #23

--- src/me/pheasn/blockown/AutoSaveTask.java
+++ src/me/pheasn/blockown/AutoSaveTask.java
@@ -3,6 +3,8 @@ package me.pheasn.blockown;
 import java.util.TimerTask;
 import java.util.logging.Level;
 
+import me.pheasn.blockown.owning.Owning.DatabaseType;
+
 public class AutoSaveTask extends TimerTask {
 	private BlockOwn plugin;
 
@@ -12,13 +14,14 @@ public class AutoSaveTask extends TimerTask {
 
 	@Override
 	public void run() {
-		if (plugin.getOwning().save()) {
-			plugin.getLogger().log(Level.FINEST,
-					Messages.getString("AutoSaveTask.0")); //$NON-NLS-1$
-		} else {
-			plugin.getLogger().log(Level.WARNING,
-					Messages.getString("AutoSaveTask.1")); //$NON-NLS-1$
+		if (plugin.getOwning().getType().equals(DatabaseType.CLASSIC)) {
+			if (plugin.getOwning().save()) {
+				plugin.getLogger().log(Level.FINEST,
+						Messages.getString("AutoSaveTask.0")); //$NON-NLS-1$
+			} else {
+				plugin.getLogger().log(Level.WARNING,
+						Messages.getString("AutoSaveTask.1")); //$NON-NLS-1$
+			}
 		}
 	}
-
 }
