a2f393cddfcae856251396c52826bcc3eb2771b3
Björn Petersen <pheasn@gmail.com>
2013-11-24 17:01:25 +0100

    Some cosmetic stuff

--- src/me/pheasn/blockown/commands/CE_Owner.java
+++ src/me/pheasn/blockown/commands/CE_Owner.java
@@ -26,18 +26,14 @@ public class CE_Owner implements CommandExecutor {
 			Block target = User.getInstance(player).getTargetBlock();
 			if (target != null) {
 				if (plugin.getOwning().getOwner(target) != null) {
-					plugin.say(player, ChatColor.GREEN,
-							Messages.getString("CE_Owner.success", plugin //$NON-NLS-1$
-									.getOwning().getOwner(target).getName()));
+					plugin.say(player, ChatColor.GREEN, Messages.getString("CE_Owner.success", plugin.getOwning().getOwner(target).getName())); //$NON-NLS-1$
 					return true;
 				} else {
-					plugin.say(player, ChatColor.GREEN,
-							Messages.getString("CE_Owner.noOwner")); //$NON-NLS-1$
+					plugin.say(player, ChatColor.GREEN, Messages.getString("CE_Owner.noOwner")); //$NON-NLS-1$
 					return true;
 				}
 			} else {
-				plugin.say(player, ChatColor.RED,
-						Messages.getString("noTargetBlock")); //$NON-NLS-1$
+				plugin.say(player, ChatColor.RED, Messages.getString("noTargetBlock")); //$NON-NLS-1$
 				return false;
 			}
 		} else {
