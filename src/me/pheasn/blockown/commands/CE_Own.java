0ed2266a3a4c4f5acfe97c18e1ccf82956fa136c
Björn Petersen <pheasn@gmail.com>
2013-12-09 15:32:07 +0100

    Fixes #51, Closes #48

--- src/me/pheasn/blockown/commands/CE_Own.java
+++ src/me/pheasn/blockown/commands/CE_Own.java
@@ -1,7 +1,12 @@
 package me.pheasn.blockown.commands;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
 
+import me.pheasn.Material;
 import me.pheasn.OfflineUser;
 import me.pheasn.Region;
 import me.pheasn.User;
@@ -12,7 +17,6 @@ import me.pheasn.blockown.BlockOwn.Setting;
 
 import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -28,16 +32,41 @@ public class CE_Own implements CommandExecutor {
 		this.plugin = plugin;
 	}
 
+	public enum Arg {
+		SELECTION("selection"),
+		MATERIAL("material"),
+		PLAYER("player");
+		
+		private String s;
+		private Arg(String s){
+			this.s = s;
+		}
+
+		/**
+		 * This is not working the other way round (String.equalsIgnoreCase(Arg)), sorry Oracle!
+		 * @param string
+		 * @return
+		 */
+		public boolean equalsIgnoreCase(String string){
+			if(string == null) return false;
+			return string.equalsIgnoreCase(s);
+		}
+
+		@Override
+		public String toString(){
+			return this.s;
+		}
+	}
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd,
-			String cmd_label, String[] args) {
+			String cmd_label, String[] argArray) {
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			if(Setting.DISABLE_OWNING_IN_WORLDS.getStringList(plugin).contains(player.getWorld().getName())){
 				plugin.say(player, ChatColor.RED, "Owning is disabled in this world");
 				return true;
 			}
-			OfflineUser user = OfflineUser.getInstance(player);
 			if (Setting.PERMISSION_NEEDED_OWN_COMMAND.getBoolean(plugin)
 					&& !player.hasPermission(Permission.OWN_COMMAND.toString())) {
 				if(!(player.getGameMode()==GameMode.CREATIVE&&player.hasPermission(Permission.OWN_COMMAND_CREATIVE.toString()))){
@@ -45,69 +74,134 @@ public class CE_Own implements CommandExecutor {
 				return true;
 				}
 			}
-			if (args.length == 0) {
-				Block target = User.getInstance(player).getTargetBlock();
-				OfflineUser owner = plugin.getOwning().getOwner(target);
-				if (owner == null) {
-					plugin.getOwning().setOwner(target, user);
-					plugin.say(player, ChatColor.GREEN, Messages.getString("CE_Own.success")); //$NON-NLS-1$
-					return true;
-				}
-				if (owner.equals(user)) {
-					plugin.say(player, ChatColor.YELLOW, Messages.getString("CE_Own.unneccessary")); //$NON-NLS-1$
-					return true;
-				} else {
-					plugin.say(player, ChatColor.RED, Messages.getString("CE_Own.ownedBy", owner.getName())); //$NON-NLS-1$
-					return false;
-				}
-			} else if (args.length == 1 && args[0].equalsIgnoreCase("selection")) { //$NON-NLS-1$
-				if (plugin.getWorldEdit() == null) {
-					plugin.tell(sender, ChatColor.RED, Messages.getString("CE_Own.selection.noWorldedit")); //$NON-NLS-1$
-					return false;
-				}
-				Selection s;
-				if ((s = plugin.getWorldEdit().getSelection(player)) != null) {
-					Region region = new Region(s.getWorld(), s.getMinimumPoint().getBlockX(), s.getMinimumPoint().getBlockY(), s.getMinimumPoint().getBlockZ(), s.getHeight(), s.getLength(), s.getWidth());
-					int failed = 0;
-					ArrayList<Block> selectedBlocks = new ArrayList<Block>();
-					for(Block block : region.getBlocks()){
-						if(block.getType().equals(Material.AIR)) continue;
-							OfflineUser owner = plugin.getOwning().getOwner(block);
-							if (owner == null) {
-								selectedBlocks.add(block);
-							} else if (!owner.equals(user)) {
-								failed += 1;
+			OfflineUser user = OfflineUser.getInstance(player);
+
+			//Getting arguments
+			List<String> args = Arrays.asList(argArray);
+			Iterator<String> argIterator = args.iterator();
+			String arg;
+			boolean selection = false, not = false;
+			ArrayList<Material> materials = null;
+			String playerName = null;
+			try{
+				while(argIterator.hasNext()){
+					arg = argIterator.next();
+
+					if(Arg.MATERIAL.equalsIgnoreCase(arg)){
+						materials = new ArrayList<Material>();
+						String next = argIterator.next();
+						if(next.equalsIgnoreCase("not")){
+							not = true;
+							continue;
+						}else{
+							Material material = Material.getMaterial(next);
+							if(material != null){
+								materials.add(material);
 							}
-					}
-					if (Setting.ECONOMY_ENABLE.getBoolean(plugin)
-							&& plugin.getEconomy() != null
-							&& Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin) > 0.0) {
-						if (plugin.getEconomy().getBalance(player.getName()) < (selectedBlocks.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin))) {
-							plugin.say(player, ChatColor.RED, Messages.getString("CE_Own.selection.noMoney",
-											(selectedBlocks.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin)), plugin.getEconomy().currencyNamePlural()));
-							return true;
-						} else {
-							plugin.say(player, ChatColor.YELLOW, Messages.getString("CE_Own.selection.howMuch",
-											(selectedBlocks.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin)), plugin.getEconomy().currencyNamePlural()));
-							plugin.getEconomy().withdrawPlayer(player.getName(), (selectedBlocks.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin)));
 						}
+						while(argIterator.hasNext()){
+							next = argIterator.next();
+							Material material = Material.getMaterial(next);
+							if(material != null){
+								materials.add(material);
+							}else{
+								break;
+							}
+						}
+						
 					}
-					for (Block selectedBlock : selectedBlocks) {
-						plugin.getOwning().setOwner(selectedBlock, user);
+
+					if(Arg.SELECTION.equalsIgnoreCase(arg)){
+						selection = true;
+						continue;
 					}
-					plugin.tell(sender, ChatColor.GREEN, Messages.getString("CE_Own.selection.success")); //$NON-NLS-1$
-					if (failed > 0) {
-						plugin.tell(sender, ChatColor.YELLOW, Messages.getString("CE_Own.selection.except", failed)); //$NON-NLS-1$
+
+					if(Arg.PLAYER.equalsIgnoreCase(arg)){
+						playerName = argIterator.next();
+						continue;
 					}
-					return true;
-				} else {
-					plugin.tell(sender, ChatColor.RED, Messages.getString("CE_Own.selection.noArea")); //$NON-NLS-1$
-					return true;
+
 				}
-			} else {
-				plugin.tell(sender, ChatColor.RED, Messages.getString("countArgs")); //$NON-NLS-1$
+			}catch(NoSuchElementException e){
+				plugin.say(player, ChatColor.RED, "You made some syntax mistake!");
 				return false;
 			}
+
+			// Getting specified player
+			OfflineUser newOwner = (playerName == null) ? user : OfflineUser.getInstance(playerName);
+			if(newOwner == null) {
+				plugin.say(player, ChatColor.RED, "You specified an invalid player");
+				return false;
+			}
+
+			// Determining amount of blocks
+			List<Block> targets = new ArrayList<Block>();
+			int others = 0;
+			int unneccessary = 0;
+			if(selection){
+				if(plugin.getWorldEdit() == null) {
+					plugin.tell(sender, ChatColor.RED, Messages.getString("CE_Own.selection.noWorldedit"));
+					return true;
+				}
+				Selection s = plugin.getWorldEdit().getSelection((Player)sender);
+				List<Block> candidateTargets = new Region(s.getWorld(), s.getMinimumPoint().getBlockX(), s.getMinimumPoint().getBlockY(), s.getMinimumPoint().getBlockZ(), s.getHeight(), s.getLength(), s.getWidth()).getBlocks();
+				for(Block block : candidateTargets){
+					if(block.getType().equals(org.bukkit.Material.AIR)) continue;
+					if(materials != null){
+						if(not){
+							if(materials.contains(block.getType())) continue;
+						}else{
+							if(!materials.contains(block.getType())) continue;
+						}
+					}
+					OfflineUser owner = plugin.getOwning().getOwner(block);
+					if(owner != null){
+						if(owner.getName().equals(newOwner.getName())){
+							unneccessary++;
+						}else{
+							others++;
+						}
+					}else{
+						targets.add(block);
+					}
+				}
+			}else{
+				targets.add(User.getInstance(player).getTargetBlock());
+			}
+
+			// Economy
+			if(selection && plugin.getEconomy() != null && Setting.ECONOMY_ENABLE.getBoolean(plugin) && Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin) != 0.0){
+				if (plugin.getEconomy().getBalance(player.getName()) < (targets.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin))) {
+					plugin.say(player, ChatColor.RED, Messages.getString("CE_Own.selection.noMoney",
+									(targets.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin)), plugin.getEconomy().currencyNamePlural()));
+					return true;
+				} else {
+					plugin.say(player, ChatColor.YELLOW, Messages.getString("CE_Own.selection.howMuch",
+									(targets.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin)), plugin.getEconomy().currencyNamePlural()));
+					plugin.getEconomy().withdrawPlayer(player.getName(), (targets.size() * Setting.ECONOMY_PRICE_OWN_SELECTION.getDouble(plugin)));
+				}
+			}
+
+			//Actually own blocks
+			for(Block block : targets){
+				plugin.getOwning().setOwner(block, newOwner);
+			}
+
+			// Tell player about the result
+			if(selection){
+				plugin.say(player, ChatColor.GREEN, Messages.getString("CE_Own.selection.success"));
+				if(others > 0 || unneccessary > 0){
+					plugin.say(player, ChatColor.YELLOW, Messages.getString("CE_Own.selection.except", others, unneccessary));
+				}
+				return true;
+			}else if(playerName == null){
+				plugin.say(player, ChatColor.GREEN, Messages.getString("CE_Own.success.own"));
+				return true;
+			}else{
+				plugin.say(player, ChatColor.GREEN, Messages.getString("CE_Own.success.other", newOwner.getName()));
+				return true;
+			}
+
 		} else {
 			plugin.con(ChatColor.RED, Messages.getString("justForPlayers")); //$NON-NLS-1$
 			return false;
